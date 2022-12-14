f868f9bc65b81616bd8dfc3adb461d463f50c06f
RaphaelStangl2 <raphael.stangl.12@gmail.com>
2022-12-14 12:25:18 +0100

    Minor fixes in BLE-Test App

---
+++ AndroidProjekte/BLE_Test/app/src/main/java/com/example/ble_test/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.ble_test
+
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ble_test.ui.theme.BLE_TestTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var bluetoothAdapter: BluetoothAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BLE_TestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    BLEApp(onBluetoothStateChanged = { showBluetoothDialog()})
+                }
+            }
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        showBluetoothDialog()
+    }
+
+    private var isBluetootDialogAlreadyShown = false
+    private fun showBluetoothDialog(){
+        if(!bluetoothAdapter.isEnabled){
+            if(!isBluetootDialogAlreadyShown){
+                val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startBluetoothIntentForResult.launch(enableBluetoothIntent)
+                isBluetootDialogAlreadyShown = true
+            }
+        }
+    }
+
+    private val startBluetoothIntentForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            isBluetootDialogAlreadyShown = false
+            if(result.resultCode != Activity.RESULT_OK){
+                showBluetoothDialog()
+            }
+        }
+}

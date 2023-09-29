34aad82b9804dea6e9a0504af9ed6fe0991339ad
berdnkadir12 <58390944+berdankadir12@users.noreply.github.com>
2023-09-29 09:40:55 +0200

    Update Car.java

--- Backend/egd-user-with-quarkus/src/main/java/at/htl/model/Car.java
+++ Backend/egd-user-with-quarkus/src/main/java/at/htl/model/Car.java
@@ -28,9 +28,15 @@ public class Car {
     private Double longitude;
     private Double latitude;
 
+    private String uuid;
+
+    @ManyToOne
+    @JoinColumn(name = "users_id")
+    private Users currentUser;
 
    /* @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 */
+
 }

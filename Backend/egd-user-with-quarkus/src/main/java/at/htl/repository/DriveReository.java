779d29c77093cd0cf85de1c107e06661772dd899
berdnkadir12 <58390944+berdankadir12@users.noreply.github.com>
2023-10-06 10:20:22 +0200

    Update DriveReository.java

--- Backend/egd-user-with-quarkus/src/main/java/at/htl/repository/DriveReository.java
+++ Backend/egd-user-with-quarkus/src/main/java/at/htl/repository/DriveReository.java
@@ -1,5 +1,31 @@
 package at.htl.repository;
 
+import at.htl.model.Drive;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
 public class DriveReository {
 
+    @Inject
+    EntityManager entityManager;
+
+    @Transactional
+    public Drive addDrive(Drive drive) {
+        entityManager.persist(drive);
+        return drive;
+    }
+
+    @Transactional
+    public void removeDrive(final long driveId) {
+        final Drive drive= findByDriveId(driveId);
+        final Drive car = findById(carId);
+        entityManager.remove(userCar);
+        entityManager.remove(car);
+    }
+
+    private Drive findByDriveId(long driveId) {
+        return entityManager.find(Drive.class, driveId);
+    }
 }

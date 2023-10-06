8769a03acb334e55df14dc164ec11eb44c613dc9
berdnkadir12 <58390944+berdankadir12@users.noreply.github.com>
2023-10-06 09:35:50 +0200

    Update DriveResource.java

--- Backend/egd-user-with-quarkus/src/main/java/at/htl/resource/DriveResource.java
+++ Backend/egd-user-with-quarkus/src/main/java/at/htl/resource/DriveResource.java
@@ -1,4 +1,11 @@
 package at.htl.resource;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
 public class DriveResource {
+    @Inject
+    EntityManager entityManager;
+
+
 }

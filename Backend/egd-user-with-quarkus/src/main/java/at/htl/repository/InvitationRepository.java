f77fdc58861c5947496e4b8ec634b2911d8693d1
RsheedAlo <71698347+RsheedAlo@users.noreply.github.com>
2023-10-10 10:21:40 +0200

    invitation resource

--- Backend/egd-user-with-quarkus/src/main/java/at/htl/repository/InvitationRepository.java
+++ Backend/egd-user-with-quarkus/src/main/java/at/htl/repository/InvitationRepository.java
@@ -2,11 +2,13 @@ package at.htl.repository;
 
 import at.htl.model.Car;
 import at.htl.model.Invitation;
+import at.htl.model.UserCar;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
 
 
 @ApplicationScoped
@@ -21,5 +23,36 @@ public class InvitationRepository {
         return invitation;
     }
 
-    
+
+
+
+
+    @Transactional
+    public void removeInv(final long invId) {
+        final Invitation invitation = findById(invId);
+        entityManager.remove(invitation);
+    }
+
+
+
+
+    public Invitation findById(long id) {
+
+        return entityManager.find(Invitation.class, id);
+    }
+
+
+
+
+    @Transactional
+    public void updateInvation(Invitation invitation) {
+        entityManager.merge(invitation);
+    }
+
+    public List<Invitation> getAllInv() {
+        return entityManager.createQuery("SELECT i FROM Invitation i", Invitation.class)
+                .getResultList();
+    }
+
+
 }

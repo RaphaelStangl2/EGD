8b44c13a49bfa20982a80c16ef33544f598809bb
RsheedAlo <71698347+RsheedAlo@users.noreply.github.com>
2024-01-16 09:51:25 +0100

    error fixed

--- AndroidProjekte/EGD/app/src/main/java/com/example/egd/ui/bottomNav/statistics/LineChartDiagram.kt
+++ AndroidProjekte/EGD/app/src/main/java/com/example/egd/ui/bottomNav/statistics/LineChartDiagram.kt
@@ -10,15 +10,21 @@ import androidx.compose.material.Text
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.yml.charts.axis.AxisData
+import co.yml.charts.common.model.AccessibilityConfig
 import co.yml.charts.common.model.Point
 import co.yml.charts.common.utils.DataUtils
 import co.yml.charts.ui.barchart.BarChart
 import co.yml.charts.ui.barchart.models.BarChartData
+import co.yml.charts.ui.barchart.models.BarData
+import co.yml.charts.ui.barchart.models.BarStyle
 import co.yml.charts.ui.linechart.LineChart
 import co.yml.charts.ui.linechart.model.GridLines
 import co.yml.charts.ui.linechart.model.IntersectionPoint
@@ -34,7 +40,7 @@ import com.example.egd.ui.DonutPieChartWithSlices
 
 @Composable
 fun LineChartDiagram(modifier: Modifier = Modifier){
-    val barChartListSize = 5
+ /*   val barChartListSize = 5
     val maxRange = 1000; // maxrange = der am meisten geld hat
     val barChartDataa = DataUtils.getBarChartData(barChartListSize, maxRange)
 
@@ -63,6 +69,13 @@ fun LineChartDiagram(modifier: Modifier = Modifier){
         showYAxis = true
 
     )
+
+*/
+
+
+//usage
+
+   // BarChart(modifier = Modifier.height(350.dp), barChartData = barChartData)
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -74,7 +87,19 @@ fun LineChartDiagram(modifier: Modifier = Modifier){
             fontWeight = FontWeight.Bold
         )
         Spacer(modifier = Modifier.height(16.dp))
-        BarChart(modifier = Modifier.height(350.dp), barChartData = barChartData)
+       // BarChart(modifier = Modifier.height(350.dp), barChartData = barChartData)
+
+
+        val dataset = listOf(
+            BarData(Point(1f, 20f, "Description 1"), Color.Blue, "Category 1", listOf(Color.LightGray, Color.Black), "Description 1"),
+            BarData(Point(2f, 35f, "Description 2"), Color.Green,"Category 2", listOf(Color.Green,Color.Green), "Description 2"),
+            BarData(Point(3f, 45f, "Description 3"), Color.Red, "Category 3", listOf(Color.Red, Color.Red), "Description 3"),
+            BarData(Point(4f, 25f, "Description 4"), Color.Yellow, "Category 4", listOf(Color.Yellow, Color.Yellow), "Description 4")
+        )
+
+
+
+        BarChart(modifier = Modifier.height(350.dp), barChartData = BarChartData(dataset))
 
     }
 

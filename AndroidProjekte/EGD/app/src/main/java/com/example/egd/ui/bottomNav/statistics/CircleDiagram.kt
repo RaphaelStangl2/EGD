2274fcde463683c464d4b47b6f6db3f423c4c88d
RaphaelStangl2 <raphael.stangl.12@gmail.com>
2024-02-01 19:34:22 +0100

    magic

--- AndroidProjekte/EGD/app/src/main/java/com/example/egd/ui/bottomNav/statistics/CircleDiagram.kt
+++ AndroidProjekte/EGD/app/src/main/java/com/example/egd/ui/bottomNav/statistics/CircleDiagram.kt
@@ -1,15 +1,7 @@
 package com.example.egd.ui.bottomNav.statistics
 
 import android.text.TextUtils
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.AlertDialog
@@ -176,15 +168,20 @@ fun CircleDiagram(modifier: Modifier = Modifier, viewModel: EGDViewModel, donutC
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
-            Text(
-                text = header,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
-            )
-            if (header == "Costs") {
-                Button(onClick = { viewModel.setShowCosts(true) }) {
-                    Text("Add Costs")
+            Row (horizontalArrangement = Arrangement.SpaceBetween){
+                Text(
+                    text = header,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                if (header == "Costs") {
+                    Button(onClick = { viewModel.setShowCosts(true) }) {
+                        Text("Add Costs")
+                    }
                 }
+            }
+
+            if (header == "Costs") {
                 if (showCostsScreen) {
                     AddCostsDialogue(viewModel = viewModel)
                 }

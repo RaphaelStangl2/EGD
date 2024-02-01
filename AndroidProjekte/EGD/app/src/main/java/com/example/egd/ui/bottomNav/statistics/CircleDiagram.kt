ae7e3e01ece72ffe8950781f92d51e4b0a4f11cf
RsheedAlo <71698347+RsheedAlo@users.noreply.github.com>
2024-02-01 16:00:23 +0100

    no drives screen

--- AndroidProjekte/EGD/app/src/main/java/com/example/egd/ui/bottomNav/statistics/CircleDiagram.kt
+++ AndroidProjekte/EGD/app/src/main/java/com/example/egd/ui/bottomNav/statistics/CircleDiagram.kt
@@ -2,9 +2,11 @@ package com.example.egd.ui.bottomNav.statistics
 
 import android.service.autofill.DateTransformation
 import android.text.TextUtils
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,7 +18,9 @@ import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -24,15 +28,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.yml.charts.common.model.PlotType
 import co.yml.charts.ui.piechart.charts.DonutPieChart
 import co.yml.charts.ui.piechart.models.PieChartConfig
 import co.yml.charts.ui.piechart.models.PieChartData
+import com.example.egd.R
 import com.example.egd.data.entities.Costs
 import com.example.egd.data.entities.Drive
 import com.example.egd.ui.DonutPieChartWithSlices
@@ -181,18 +190,18 @@ fun CircleDiagram(modifier: Modifier = Modifier, viewModel: EGDViewModel, donutC
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold
             )
-            if (header == "Costs"){
-                Button(onClick = {viewModel.setShowCosts(true)}){
+            if (header == "Costs") {
+                Button(onClick = { viewModel.setShowCosts(true) }) {
                     Text("Add Costs")
                 }
-                if (showCostsScreen){
+                if (showCostsScreen) {
                     AddCostsDialogue(viewModel = viewModel)
                 }
                 Spacer(modifier = Modifier.height(16.dp))
             }
             Spacer(modifier = Modifier.height(16.dp))
 
-            if (donutChartData.slices.isNotEmpty() && donutChartConfig != null ) {
+            if (donutChartData.slices.isNotEmpty() && donutChartConfig != null) {
                 DonutPieChart(
                     modifier = modifier,
                     donutChartData,
@@ -202,10 +211,40 @@ fun CircleDiagram(modifier: Modifier = Modifier, viewModel: EGDViewModel, donutC
                         clickedSlice = slice
                     }
                 )
+                DonutPieChartWithSlices(
+                    slices = donutChartData.slices,
+                    donutChartConfig = donutChartConfig
+                )
+            } else {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_car_repair_24),
+                        contentDescription = "Car Icon",
+                        modifier = Modifier
+                            .size(96.dp)
+                            .padding(bottom = 16.dp),
+                        tint = Color.Gray
+                    )
+                    Text(
+                        text = "This car hasn't had any drives yet. Drives will be automatically added if any user of the car drives more than 1 km.",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        style = TextStyle(
+                            color = Color.Gray,
+                            fontSize = 16.sp
+                        )
+                    )
+                }
             }
-        DonutPieChartWithSlices(slices = donutChartData.slices, donutChartConfig = donutChartConfig)
-    }
 
-}
+        }}
 
 
